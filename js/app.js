f8d47276364c3c7e7f42a12a989a6f7aa67ff764
electronicsmf <electronicsmf@aol.com>
2017-12-19 14:16:47 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+angular.module('myApp',[]).
+controller('ControllerA',function($scope){
+    $scope.title = 'Angular Power';
+    $scope.bannerh = 'Angular Templates';
+    $scope.message = 'Hello from this App';
+    $scope.bannerp = 'Dynamic Home page';
+    $scope.fheading = 'Core Angular';
+    $scope.theading = 'HTML template ';
+    $scope.sheading = 'Dependency injection';
+    $scope.lheading ='Angular 2 was a compiler';
+
+});
+
+
+ 
